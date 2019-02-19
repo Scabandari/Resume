50da148d57e1fdc677fa024b73c477741e5ffd21
Scabandari <ryan3nichols@gmail.com>
2019-02-19 15:08:24 -0500

    Closes #8, closes #6. Navigation buttons working great. Basic footer is made. Could use some updates in the future

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import React, { Component } from "react";
 import Header from './components/Header';
+import Footer from './components/Footer';
+
 import Landing from './components/Landing';
 import Projects from './components/Projects';
 import PDF from './components/Projects/PDFs/Pdf';
 
-const Dashboard = () => <h1>Dashboard</h1>;
+const Skills = () => <h1>Skills Comming Soon!</h1>;
+const Technologies = () => <h1>Technologies</h1>;
 
 class App extends Component {
     render(){
@@ -13,10 +16,12 @@ class App extends Component {
             <BrowserRouter >
                 <div>
                     <Header />
-                    <Route path="/dashboard" exact component={Dashboard} />
                     <Route path="/" exact component={Landing} />
                     <Route path="/projects" exact component={Projects} />
                     <Route path="/pdf" exact component={PDF} />
+                    <Route path="/skills" exact component={Skills} />
+                    <Route path="/techs" exact component={Technologies} />
+                    <Footer />
                 </div>
             </BrowserRouter>
         )
--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -4,6 +4,7 @@ import classNames from 'classnames';
 import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import purple from '@material-ui/core/colors/purple';
+import { Link } from 'react-router-dom';
 import green from '@material-ui/core/colors/green';
 //'#493C55'
 const styles = theme => ({
@@ -21,15 +22,20 @@ const styles = theme => ({
 });
 
 function HeaderButton(props) {
-    const { classes } = props;
+    const { classes, name, link } = props;
+    // console.log(`ref: ${link}`);
+    // console.log(`name: ${name}`);
 
+    // const { name, ref } = props;
     return (
         <Button
+            component={ Link }
+            to={link}
             variant="contained"
             color="primary"
             className={classNames(classes.margin, classes.cssRoot)}
         >
-            Custom CSS
+            { name }
         </Button>
     );
 }
