dbf4fad6541bc6c0bab6a8599d9afb4f81af591c
Scabandari <ryan3nichols@gmail.com>
2019-11-01 11:46:32 -0400

    Footer didn't look right on skills page but it's fixed now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -9,39 +9,52 @@ import EmailForm from './components/EmailForm';
 import SkillsExpansionPanel from './containers/SkillsContainer/SkillsExpansionPanel';
 import TechnologiesExpansionPanel from './containers/TechnologiesContainer/TechnologiesExpansionPanel';
 
+const styles = {
+  body: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+  },
+  siteContent: {
+    flex: '1 0 auto',
+    padding: 'var(--space) var(--space) 0',
+    width: '100%'
+  }
+};
+
 class App extends Component {
   render() {
-    const styles = {
-      body: {
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh'
-      },
-      siteContent: {
-        flex: '1 0 auto',
-        padding: 'var(--space) var(--space) 0',
-        width: '100%'
-      }
-    };
     return (
       <BrowserRouter>
         <div style={styles.body}>
-          <div style={styles.siteContent}>
-            <Header />
-            <Route path='/' exact component={Landing} />
-            <Route path='/projects' exact component={Projects} />
-            <Route path='/pdf' exact component={PDF} />
-            <Route path='/skills' exact component={SkillsExpansionPanel} />
-            <Route path='/techs' exact component={TechnologiesExpansionPanel} />
-            <Route path='/contact' exact component={EmailForm} />
-            <Footer />
-          </div>
+          <Switch>
+            <Route exact path='/' component={LandingContainer} />
+            <Route component={DefaultContainer} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
   }
 }
 
+const LandingContainer = () => (
+  <div>
+    <Route path='/' exact component={Landing} />
+  </div>
+);
 
+const DefaultContainer = () => (
+  <div>
+    <Header />
+    <div style={styles.siteContent}>
+      <Route path='/projects' exact component={Projects} />
+      <Route path='/pdf' exact component={PDF} />
+      <Route path='/skills' exact component={SkillsExpansionPanel} />
+      <Route path='/techs' exact component={TechnologiesExpansionPanel} />
+      <Route path='/contact' exact component={EmailForm} />
+    </div>
+    <Footer />
+  </div>
+);
 
 export default App;
--- src/containers/SkillsContainer/SkillsExpansionPanel.jsx
+++ src/containers/SkillsContainer/SkillsExpansionPanel.jsx
@@ -1,19 +1,17 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 import GenericExpansionPanel from '../../components/GenericExpansionPanel';
 import skills from './descriptions';
 
-
 class SkillsExpansionPanel extends Component {
-    my_skills = [
-        skills.people,
-        skills.leadership,
-        skills.foosball,
-    ];
+  my_skills = [skills.people, skills.leadership, skills.foosball];
 
-
-    render() {
-        return <GenericExpansionPanel topics={this.my_skills} />
-    }
+  render() {
+    return (
+      <div style={{ minHeight: '70vh' }}>
+        <GenericExpansionPanel topics={this.my_skills} />
+      </div>
+    );
+  }
 }
 
 export default SkillsExpansionPanel;
