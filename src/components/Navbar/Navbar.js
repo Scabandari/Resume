d4893303cdd2b07043d500621eabf25b2843a7f2
Scabandari <ryan3nichols@gmail.com>
2019-11-04 10:45:00 -0500

    Closes #45 Active property on Navbar now being set properly

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Icon, Menu } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
@@ -6,55 +6,77 @@ import { withRouter } from 'react-router-dom';
 import { screenSize } from '../hoc';
 import './Navbar.scss';
 
-class MenuExampleSecondary extends Component {
-  state = { activeItem: '' };
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-
-  render() {
-    //console.log(`this.props: ${JSON.stringify(this.props)}`);
-    const { activeItem } = this.state;
-    const { width } = this.props;
-    const { pathname } = this.props.location;
-
-    return (
-      <div className='navbar-container'>
-        <Menu icon inverted size='massive'>
-          <Menu.Item
-            header
-            name='Ryan Nichols'
-            //active={activeItem === 'home'}
-            //onClick={this.handleItemClick}
-          />
-
-          <Menu.Item disabled as={NavLink} to='/' name='avsddf'>
-            <Icon name='home' />
-          </Menu.Item>
-
-          {width > 500 && (
-            <Menu.Menu position='right'>
-              <Menu.Item as={NavLink} to='/projects' name='projects'>
-                <p>Projects</p>
-              </Menu.Item>
-              <Menu.Item as={NavLink} to='/techs' name='techs'>
-                <p>Tech</p>
-              </Menu.Item>
-              <Menu.Item as={NavLink} to='/skills' name='skills'>
-                <p>Skills</p>
-              </Menu.Item>
-            </Menu.Menu>
-          )}
-        </Menu>
-      </div>
-    );
+const getActiveItem = pathName => {
+  switch (pathName) {
+    case '/techs':
+      return 'techs';
+    case '/projects':
+      return 'projects';
+    case '/skills':
+      return 'skills';
+    default:
+      return '';
   }
-}
+};
 
-// const Navbar = props => {
-//   return <MenuExampleSecondary width={props.width} />;
-// };
-//const Navbar = props => <MenuExampleSecondary {...props} />;
+const MenuExampleSecondary = props => {
+  const { width } = props;
+  const { pathname } = props.location;
+
+  const [activeItem, setActiveItem] = useState(getActiveItem(pathname));
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+  return (
+    <div className='navbar-container'>
+      <Menu icon inverted size='massive'>
+        <Menu.Item
+          header
+          name='Ryan Nichols'
+        />
+
+        <Menu.Item disabled as={NavLink} to='/' name='avsddf'>
+          <Icon name='home' />
+        </Menu.Item>
+
+        {width > 500 && (
+          <Menu.Menu position='right'>
+            <Menu.Item
+              as={NavLink}
+              to='/projects'
+              name='projects'
+              active={activeItem === 'projects'}
+              onClick={handleItemClick}
+            >
+              <p>Projects</p>
+            </Menu.Item>
+            <Menu.Item
+              as={NavLink}
+              to='/techs'
+              name='techs'
+              active={activeItem === 'techs'}
+              onClick={handleItemClick}
+            >
+              <p>Tech</p>
+            </Menu.Item>
+            <Menu.Item
+              as={NavLink}
+              to='/skills'
+              name='skills'
+              active={activeItem === 'skills'}
+              onClick={handleItemClick}
+            >
+              <p>Skills</p>
+            </Menu.Item>
+          </Menu.Menu>
+        )}
+      </Menu>
+    </div>
+  );
+};
+
+
 const Navbar = withRouter(props => <MenuExampleSecondary {...props} />);
 
-//export default withRouter(screenSize(Navbar));
+
 export default screenSize(Navbar);
