062be431a215be36c50b8ddf9d426ac6490be498
Scabandari <ryan3nichols@gmail.com>
2019-11-15 11:28:32 -0500

    Adding file to gitignore

--- src/components/TechnologiesExpansionPanel/TechnologiesExpansionPanel.jsx
+++ src/components/TechnologiesExpansionPanel/TechnologiesExpansionPanel.jsx
@@ -15,18 +15,18 @@ import {
 } from './descriptions';
 
 const my_techs = [
-    js,
-    python,
-    java,
-    cpp,
-    sql,
-    machine_learning,
-    cloud,
-    git,
-    block_chain,
-    css,
-    bash
-  ];
+  js,
+  python,
+  java,
+  cpp,
+  sql,
+  machine_learning,
+  cloud,
+  git,
+  block_chain,
+  css,
+  bash
+];
 const TechnologiesExpansionPanel = () => {
   return <GenericExpansionPanel topics={my_techs} />;
 };
