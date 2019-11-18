17adf7d45033c49681b802796f6d48c979338fca
Scabandari <ryan3nichols@gmail.com>
2019-11-18 15:16:04 -0500

    Closes #64 Sidebar now closes if user drags the page width beyond where the hamburger menu normally appears

--- src/hooks/useScreenWidth.js
+++ src/hooks/useScreenWidth.js
@@ -1,9 +1,19 @@
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { SET_SIDEBAR_SHOWING } from '../actions/types';
 
 const useScreenWidth = () => {
   const [width, setWidth] = useState(window.innerWidth);
+  const sidebarIsShowing = useSelector(state => state.navbar.sidebarIsShowing);
+  const dispatch = useDispatch();
 
-  const handleResize = () => setWidth(window.innerWidth);
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+    if (window.innerWidth > 515 && sidebarIsShowing) {
+      dispatch({ type: SET_SIDEBAR_SHOWING, payload: false });
+    }
+  };
 
   useEffect(() => {
     window.addEventListener('resize', handleResize);
